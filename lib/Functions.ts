43ab9fc09962c7597eefe7883a1528feceb5d5e8
simon wik <simonornbrant@gmail.com>
2024-02-16 15:06:36 +0100

    changed generate_id function again

--- lib/Functions.ts
+++ lib/Functions.ts
@@ -6,17 +6,24 @@ export function create_project(title: string): Project {
     return { title: title, task_table: create_empty_hash(43, quadratic_probing_function(hash_function)) };
 }
 
-function generate_id() {
-    return Date.now();
+function generate_id(is_subtask: boolean) {
+    const num = Date.now();
+    return is_subtask ? parseInt("9".concat(num.toString()))
+                      : parseInt("1".concat(num.toString()));
+    
+    
 }
 
 export function create_task(title: string, description: string, priority: number): Task {
     return { 
         title: title, 
-        id: generate_id(),
+        id: generate_id(false),
         description: description,
         subtasks: undefined,
         status: false,
         priority: priority
     };
 }
+
+console.log(generate_id(false));
+console.log(generate_id(true));
