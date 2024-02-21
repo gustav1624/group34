b53d3cdf864f1206e13e222a921f601dd45b8632
setterholm <wilmer.setterholm.0551@student.uu.se>
2024-02-21 16:11:29 +0100

    added a non-working test

--- lib/tests.test.ts
+++ lib/tests.test.ts
@@ -1,10 +1,16 @@
-//testcases
+import { Project, Task, SubTask, User, HashFunction, hash_function, ProbingFunction, 
+    TaskStorage, prompt, quadratic_probing_function, create_empty_hash, ph_delete, ph_insert, 
+    ph_lookup, 
+    Category} from "./exports";
 
-import { generate_id } from "./Functions";
+import { create_project, generate_id } from "./Functions";
 
 
 test("Create project, create task", () => {
-
+    const test_project = create_project("Project 1");
+    const test_task_table = create_empty_hash(43, quadratic_probing_function(hash_function));
+    const test_project_expected = { title: "Project 1", task_table: test_task_table, task_ids: [], categories: [] };
+    expect(test_project_expected).toMatchObject(test_project);
 });
 
 test("Generate ID", () => {
