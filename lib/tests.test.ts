69e1e4506306756d52215e51b5dc1c8901e93885
setterholm <wilmer.setterholm.0551@student.uu.se>
2024-02-23 10:50:17 +0100

    added create task, create project

--- lib/tests.test.ts
+++ lib/tests.test.ts
@@ -3,14 +3,28 @@ import { Project, Task, SubTask, User, HashFunction, hash_function, ProbingFunct
     ph_lookup, 
     Category} from "./exports";
 
-import { create_project, generate_id } from "./Functions";
+import { create_project, create_task, generate_id } from "./Functions";
 
 
-test("Create project, create task", () => {
+test("Create project", () => {
     const test_project = create_project("Project 1");
     const test_task_table = create_empty_hash(43, quadratic_probing_function(hash_function));
     const test_project_expected = { title: "Project 1", task_table: test_task_table, task_ids: [], categories: [] };
-    expect(test_project_expected).toMatchObject(test_project);
+    expect(JSON.stringify(test_project_expected)).toEqual(JSON.stringify(test_project));
+});
+
+test("Create task", () => {
+    const test_task = create_task("Task 1", "Task instructions", 2);
+    test_task.id = 1;
+    const test_task_expected = { 
+        title: "Task 1", 
+        id: 1, 
+        description: "Task instructions", 
+        subtasks: undefined, 
+        status: false, 
+        priority: 2 
+    };
+    expect(JSON.stringify(test_task_expected)).toEqual(JSON.stringify(test_task));
 });
 
 test("Generate ID", () => {
