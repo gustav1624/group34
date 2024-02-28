87684c2a0773f8324758a66bf8bb240252698a39
setterholm <wilmer.setterholm.0551@student.uu.se>
2024-02-28 10:39:31 +0100

    moved main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { Project, Task, SubTask, User, HashFunction, hash_function, ProbingFunction, 
     TaskStorage, prompt, quadratic_probing_function, create_empty_hash, ph_delete, ph_insert, 
-    ph_lookup, Category } from "./exports";
+    ph_lookup, Category } from "./lib/exports";
 
 import { add_subtask, create_project, create_task, generate_id, alphabetical_sort, priority_sort, 
     empty_category, get_task_progress, edit_task, assign_task, complete_task, filter_completed, 
-    task_to_project, access_task, remove_task_from_project, view_task } from "./Functions";
+    task_to_project, access_task, remove_task_from_project, view_task } from "./lib/Functions";
 
