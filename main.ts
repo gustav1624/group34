fd0fa10e2347bdb742d04baa37a3f98dd8ce6c99
setterholm <wilmer.setterholm.0551@student.uu.se>
2024-02-28 11:37:27 +0100

    added menu and choose

--- main.ts
+++ main.ts
@@ -6,3 +6,45 @@ import { add_subtask, create_project, create_task, generate_id, alphabetical_sor
     empty_category, get_task_progress, edit_task, assign_task, complete_task, filter_completed, 
     task_to_project, access_task, remove_task_from_project, view_task } from "./lib/Functions";
 
+function choose(alternatives: Array<string>): number {
+    let input = 0;
+    let numbers = [];
+    for (let i = 0; i < alternatives.length; i++) {
+        numbers.push(i + 1); 
+    }
+
+    while (numbers.filter((n) => n !== input).length === numbers.length) {
+        console.log("");
+        for (let i = 0; i < alternatives.length; i++) {
+            console.log(i + 1, alternatives[i]);
+        }
+        input = +prompt("Choose an option: ");
+    }
+    return input;
+}
+
+function menu() {
+    console.log("");
+    console.log("PROJECT PLANNER");
+    console.log("");
+    console.log("MAIN MENU");
+
+    const choice = choose(["Open project", "Create new project"]);
+    if (choice === 1) {
+        open_project();
+    }
+    else if (choice === 2) {
+        create_new_project();
+    }
+    
+}
+
+function open_project() {
+    console.log("opening project");
+}
+
+function create_new_project() {
+    console.log("creating a new project");
+}
+
+menu();
